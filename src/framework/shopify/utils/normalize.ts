a046ac446ec5e5ed727075f50dc9037e8d8c8662
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-09-26 12:18:34 +0300

    Normalize product images

--- src/framework/shopify/utils/normalize.ts
+++ src/framework/shopify/utils/normalize.ts
@@ -1,7 +1,15 @@
 
 import {
+    ImageEdge,
     Product as ShopifyProduct
   } from "../schema"
+  
+  const normalizeProductImages = ({edges}: {edges: Array<ImageEdge>}) =>
+    edges.map(({node: { originalSrc: url, ...rest}}) => ({
+        url: `/images/${url}`,
+        ...rest }
+    ))
+  
   
   export function normalizeProduct(productNode: ShopifyProduct): any {
     const {
@@ -10,6 +18,7 @@ import {
       handle,
       vendor,
       description,
+      images: imageConnection,
       ...rest
     } = productNode
   
@@ -20,6 +29,7 @@ import {
       description,
       path: `/${handle}`,
       slug: handle.replace(/^\/+|\/+$/g, ""),
+      images: normalizeProductImages(imageConnection),
       ...rest
     }
   
