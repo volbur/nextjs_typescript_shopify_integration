d0118cc5d6dfb632d387cafdbb3ada63773370da
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-09-26 10:32:31 +0300

    Get all products separation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
-export default function Home() {
+
+import type { InferGetStaticPropsType } from "next"
+import getAllproducts from "../framework/shopify/product/get-all-products"
+
+export async function getStaticProps() {
+  const products = await getAllproducts()
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
+
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
     <div>
-      Hello E-commerce
+      { products }
     </div>
   )
 }
