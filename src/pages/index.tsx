7e3d33a139498bc84d9804a7607863a7a20b719e
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-09 17:15:54 +0300

    Product Card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 
 import type { InferGetStaticPropsType } from "next"
 import getAllproducts from "@framework/product/get-all-products"
-// import { getConfig } from "@framework/api/config"
 import {getConfig} from "@framework/api/config"
 import { Layout } from "@components/common"
+import { ProductCard } from "@components/product"
 
 export async function getStaticProps() {
   const config = getConfig()
@@ -23,7 +23,12 @@ export default function Home({
 
   return (
     <div className="root">
-      { JSON.stringify(products) }
+      { products.slice(0,3).map(product =>
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
+      )}
     </div>
   )
 }
