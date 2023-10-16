62e4668d5ad6143d111cd10f3dbac3ae2de7b625
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-16 16:14:48 +0300

    Marquee

---
+++ src/components/ui/Marquee/Marquee.tsx
@@ -0,0 +1,16 @@
+import { ReactNode, FC } from "react";
+import s from "./Marquee.module.css";
+
+interface Props {
+    children: ReactNode[]
+}
+
+const Marquee: FC<Props> = ({children}) => {
+    return (
+        <div className={s.root}>
+            {children}
+        </div>
+    )
+}
+
+export default Marquee
