4a05cb07b67f3a1343a2ea60b3a7560a7e73ad3f
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-25 16:30:47 +0300

    Get Product function

---
+++ src/framework/shopify/product/get-product.ts
@@ -0,0 +1,15 @@
+
+import { ApiConfig } from "@common/types/api";
+
+
+const getProduct = async (config: ApiConfig): Promise<any> => {
+
+  return {
+    product: {
+      name: "MY super product",
+      slug: "my-super-product"
+    }
+  }
+}
+
+export default getProduct
--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,43 +1,46 @@
-
-
-import { Layout } from "@components/common"
-import { getConfig } from "@framework/api/config"
-import getAllProductsPaths from "@framework/product/get-all-products-paths"
-import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next"
+import { Layout } from "@components/common";
+import { getConfig } from "@framework/api/config";
+import { getAllProductsPaths, getProduct } from "@framework/product";
+import {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
 
 // fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
-  const config = getConfig()
-  const { products } = await getAllProductsPaths(config)
+  const config = getConfig();
+  const { products } = await getAllProductsPaths(config);
 
   return {
-    paths: products.map(p => ({params: {slug: p.slug}})),
-    fallback: false
-  }
-}
+    paths: products.map((p) => ({ params: { slug: p.slug } })),
+    fallback: false,
+  };
+};
 
 // provide product spefici data to the page
 export const getStaticProps = async ({
-  params }: GetStaticPropsContext<{slug: string}>
-) => {
+  params,
+}: GetStaticPropsContext<{ slug: string }>) => {
+  const config = getConfig();
+  const { product } = await getProduct(config);
+
   return {
     props: {
-      product: {
-        slug: params?.slug
-      }
-    }
-  }
-}
+      product,
+    },
+  };
+};
 
 export default function ProductSlug({
-  product }: InferGetStaticPropsType<typeof getStaticProps>
-) {
+  product,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div>
+      {product.name}
       {product.slug}
     </div>
-  )
+  );
 }
 
-
-ProductSlug.Layout = Layout
+ProductSlug.Layout = Layout;
