b031ab5b80823e647918080c469b41181a4a583b
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-25 16:40:27 +0300

    Get product query

--- src/framework/shopify/product/get-product.ts
+++ src/framework/shopify/product/get-product.ts
@@ -1,15 +1,23 @@
 
+
 import { ApiConfig } from "@common/types/api";
+import { getProductQuery } from "@framework/utils";
 
 
 const getProduct = async (config: ApiConfig): Promise<any> => {
 
-  return {
-    product: {
-      name: "MY super product",
-      slug: "my-super-product"
+    const { data } = await config.fetch<any>({
+        query: getProductQuery, url: config.apiUrl
+    })
+
+    console.log(JSON.stringify(data, null, 2))
+
+    return {
+        product: {
+            name: "MY super product",
+            slug: "my-super-product"
+        }
     }
-  }
 }
 
 export default getProduct
--- src/framework/shopify/utils/queries/index.ts
+++ src/framework/shopify/utils/queries/index.ts
@@ -1,1 +1,2 @@
 export { getAllProductsQuery } from "./get-all-products"
+export { default as getProductQuery } from "./get-product"
