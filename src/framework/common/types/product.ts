b3af3bb3d1f2c89a89adcad5690278e93929454e
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-27 12:31:46 +0300

    variant and option type

--- src/framework/common/types/product.ts
+++ src/framework/common/types/product.ts
@@ -1,21 +1,39 @@
 
 export interface ProductImage {
-    url: string
-    alt?: string
-  }
+  url: string
+  alt?: string
+}
 
-  export interface ProductPrice {
-    value: number
-    currencyCode: "USD" | "EUR" | string
-  }
-  
-  export interface Product {
-    id: string
-    name: string
-    description: string
-    slug: string
-    path: string
-    images: ProductImage[]
-    price: ProductPrice
-  }
-  
+export interface ProductPrice {
+  value: number
+  currencyCode: "USD" | "EUR" | string
+}
+
+export interface ProductOptionValues {
+  label: string
+  hexColor?: string
+}
+
+export interface ProductOption {
+  id: string
+  displayName: string
+  values: ProductOptionValues[]
+}
+
+export interface ProductVariant {
+  id: string
+  name: string
+  options: ProductOption[]
+}
+
+export interface Product {
+  id: string
+  name: string
+  description: string
+  slug: string
+  path: string
+  images: ProductImage[]
+  price: ProductPrice,
+  options: ProductOption[],
+  variants: ProductVariant[]
+}
--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -6,6 +6,7 @@ import {
   GetStaticPropsContext,
   InferGetStaticPropsType,
 } from "next";
+import { Container } from "@components/ui";
 
 // fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -39,7 +40,49 @@ export const getStaticProps = async ({
 export default function ProductSlug({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div>{JSON.stringify(product, null, 2)}</div>;
+  return (
+    <Container>
+      <p>id: {product?.id}</p>
+      <p>name: {product?.name}</p>
+      <p>price value: {product?.price.value}</p>
+      <p>price currency: {product?.price.currencyCode}</p>
+      <p>description: {product?.description}</p>
+
+      <h1 className="mb-4">OPTIONS</h1>
+      <div>
+        {product?.options.map((option) => (
+          <div>
+            <p>Name: {option.displayName}</p>
+            {option.values.map((value) => (
+              <div>
+                <p>Label: {value.label}</p>
+                <p>Hex Color: {value.hexColor}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <h1 className="mb-4">VARIANTS</h1>
+      <div>
+        {product?.variants.map((variant) => (
+          <div>
+            <p>Variant Name: {variant.name}</p>
+            {variant.options.map((vo) => (
+              <div>
+                <p>Name: {vo.displayName}</p>
+                {vo.values.map((value) => (
+                  <div>
+                    <p>Label: {value.label}</p>
+                    <p>Hexcolot: {value.hexColor}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 }
 
 ProductSlug.Layout = Layout;
