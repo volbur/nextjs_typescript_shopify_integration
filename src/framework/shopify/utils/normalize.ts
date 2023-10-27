0e98e6d6a47a051c27fd004c86762ac40a5dc2dc
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-27 12:24:54 +0300

    Normalize options

--- src/framework/shopify/utils/normalize.ts
+++ src/framework/shopify/utils/normalize.ts
@@ -1,48 +1,65 @@
 
 import {
-    ImageEdge,
-    MoneyV2,
-    Product as ShopifyProduct
-  } from "../schema"
-  
-  import { Product } from "@common/types/product"
-  
-const normalizeProductImages = ({edges}: {edges: Array<ImageEdge>}) =>
-  edges.map(({node: { originalSrc: url, ...rest}}) => ({
-      url: `/images/${url}`,
-      ...rest }
+  ImageEdge,
+  MoneyV2,
+  Product as ShopifyProduct,
+  ProductOption
+} from "../schema"
+
+import { Product } from "@common/types/product"
+
+const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) =>
+  edges.map(({ node: { originalSrc: url, ...rest } }) => ({
+    url: `/images/${url}`,
+    ...rest
+  }
   ))
 
-const normalizeProductPrice = ({currencyCode, amount}: MoneyV2) => ({
+const normalizeProductPrice = ({ currencyCode, amount }: MoneyV2) => ({
   value: +amount,
   currencyCode
 })
-  
-  
-  export function normalizeProduct(productNode: ShopifyProduct): Product {
-    const {
-      id,
-      title: name,
-      handle,
-      vendor,
-      description,
-      images: imageConnection,
-      priceRange,
-      ...rest
-    } = productNode
-  
-    const product = {
-      id,
-      name,
-      vendor,
-      description,
-      path: `/${handle}`,
-      slug: handle.replace(/^\/+|\/+$/g, ""),
-      images: normalizeProductImages(imageConnection),
-      price: normalizeProductPrice(priceRange.minVariantPrice),
-      ...rest
-    }
-  
-    return product
+
+const normalizeProductOption = ({
+  id,
+  values,
+  name: displayName
+}: ProductOption) => {
+
+  console.log("ID ", id)
+  console.log("NAME ", displayName)
+  console.log("VALUES ", values)
+
+  return {}
+}
+
+export function normalizeProduct(productNode: ShopifyProduct): Product {
+  const {
+    id,
+    title: name,
+    handle,
+    vendor,
+    description,
+    images: imageConnection,
+    priceRange,
+    options,
+    ...rest
+  } = productNode
+
+  const product = {
+    id,
+    name,
+    vendor,
+    description,
+    path: `/${handle}`,
+    slug: handle.replace(/^\/+|\/+$/g, ""),
+    images: normalizeProductImages(imageConnection),
+    price: normalizeProductPrice(priceRange.minVariantPrice),
+    options: options ?
+      options.filter(o => o.name !== "Title")
+        .map(o => normalizeProductOption(o)) : [],
+    ...rest
   }
-  
+
+  return product
+}
