f5cb25347a2f6aefcc41d6525022edbc7d177539
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-30 14:08:24 +0200

    Sliding in keen slider

--- src/components/product/ProductSlider/ProductSlider.tsx
+++ src/components/product/ProductSlider/ProductSlider.tsx
@@ -1,30 +1,41 @@
 import React, { FC, ReactNode, Children, isValidElement } from "react";
 import s from "./ProductSlider.module.css";
+import { useKeenSlider } from "keen-slider/react";
 
 interface LayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const ProductSlider: FC<LayoutProps> = ({ children }) => {
+  const [sliderRef, _] = useKeenSlider({
+    initial: 0,
+    loop: true,
+    slideChanged(s) {
+      console.log("changing to slide: ", s.details().relativeSlide);
+    },
+  });
+
   return (
     <div className={s.root}>
-      <div className="keen-slider h-full transition-opacity">
-        { Children.map(children, child => {
+      <div
+        ref={sliderRef as any}
+        className="keen-slider h-full transition-opacity"
+      >
+        {Children.map(children, (child) => {
           if (isValidElement(child)) {
-            // return {
-            //   ...child,
-            //   props: {
-            //     ...child.props,
-            //     className: "keen-slider__slide"
-            //   }
-            // }
-
-            return React.cloneElement(child, {className: "keen-slider__slide"} as React.HTMLProps<HTMLDivElement>)
+            return {
+              ...child,
+              props: {
+                ...child.props,
+                className: `${
+                  child.props.className ? `${child.props.className}` : ""
+                } keen-slider__slide`,
+              },
+            };
           }
 
-          return child
-        }
-        )}
+          return child;
+        })}
       </div>
     </div>
   );
