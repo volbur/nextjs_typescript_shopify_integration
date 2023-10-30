d4c6a0d642a12b60c20864e2d12da603690efcba
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-30 14:26:18 +0200

    UI Button

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,7 +1,7 @@
 import cn from "classnames";
 import { FC } from "react";
 import s from "./ProductView.module.css";
-import { Container } from "@components/ui";
+import { Container, Button } from "@components/ui";
 import Image from "next/image";
 import { Product } from "@common/types/product";
 import { ProductSlider } from "@components/product";
@@ -13,7 +13,7 @@ interface Props {
 const ProductView: FC<Props> = ({ product }) => {
   return (
     <Container>
-      <div className={cn(s.root, "fit")}>
+      <div className={cn(s.root, "fit", "mb5")}>
         <div className={cn(s.productDisplay, "fit")}>
           <div className={s.nameBox}>
             <h1 className={s.name}>{product.name}</h1>
@@ -49,13 +49,9 @@ const ProductView: FC<Props> = ({ product }) => {
             </div>
           </section>
           <div>
-            <button
-              onClick={() => {}}
-              aria-label="Add to Cart"
-              className={s.button}
-            >
+            <Button>
               Add to Cart
-            </button>
+            </Button>
           </div>
         </div>
       </div>
