1c04f6c0ccdc6ecc3c68c4889888bc0c85dad3b0
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-30 13:57:02 +0200

    Keen slider

--- src/components/product/ProductSlider/ProductSlider.tsx
+++ src/components/product/ProductSlider/ProductSlider.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import React, { FC, ReactNode, Children, isValidElement } from "react";
 import s from "./ProductSlider.module.css";
 
 interface LayoutProps {
@@ -8,7 +8,24 @@ interface LayoutProps {
 const ProductSlider: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={s.root}>
-      <div className="h-full transition-opacity">{children}</div>
+      <div className="keen-slider h-full transition-opacity">
+        { Children.map(children, child => {
+          if (isValidElement(child)) {
+            // return {
+            //   ...child,
+            //   props: {
+            //     ...child.props,
+            //     className: "keen-slider__slide"
+            //   }
+            // }
+
+            return React.cloneElement(child, {className: "keen-slider__slide"} as React.HTMLProps<HTMLDivElement>)
+          }
+
+          return child
+        }
+        )}
+      </div>
     </div>
   );
 };
