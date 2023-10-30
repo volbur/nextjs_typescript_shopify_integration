a5bf7a5573080a56677b1741af7deaaa2bb894ba
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-10-30 15:10:26 +0200

    Swatch

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,26 +1,22 @@
-
-import cn from 'classnames'
-import { FC } from 'react'
-import s from './ProductView.module.css'
-import { Container, Button } from '@components/ui'
-import Image from "next/image"
-import { Product } from '@common/types/product'
-import { ProductSlider } from "@components/product"
+import cn from "classnames";
+import { FC } from "react";
+import s from "./ProductView.module.css";
+import { Container, Button } from "@components/ui";
+import Image from "next/image";
+import { Product } from "@common/types/product";
+import { ProductSlider, Swatch } from "@components/product";
 
 interface Props {
-  product: Product
+  product: Product;
 }
 
 const ProductView: FC<Props> = ({ product }) => {
-
   return (
     <Container>
-      <div className={cn(s.root, 'fit', "mb-5")}>
-        <div className={cn(s.productDisplay, 'fit')}>
+      <div className={cn(s.root, "fit", "mb-5")}>
+        <div className={cn(s.productDisplay, "fit")}>
           <div className={s.nameBox}>
-            <h1 className={s.name}>
-              {product.name}
-            </h1>
+            <h1 className={s.name}>{product.name}</h1>
             <div className={s.price}>
               {product.price.value}
               {` `}
@@ -28,7 +24,7 @@ const ProductView: FC<Props> = ({ product }) => {
             </div>
           </div>
           <ProductSlider>
-            { product.images.map(image =>
+            {product.images.map((image) => (
               <div key={image.url} className={s.imageContainer}>
                 <Image
                   className={s.img}
@@ -39,38 +35,41 @@ const ProductView: FC<Props> = ({ product }) => {
                   quality="85"
                 />
               </div>
-            )}
+            ))}
           </ProductSlider>
         </div>
         <div className={s.sidebar}>
           <section>
-            { product.options.map(option =>
-              <div key={product.id} className="pb-4">
+            {product.options.map((option) => (
+              <div key={option.id} className="pb-4">
                 <h2 className="uppercase font-medium">{option.displayName}</h2>
                 <div className="flex flex-row py-4">
-                  { option.values.map(optValue =>
-                    <div key={`${option.id}-${optValue.label}`}>
-                      {optValue.label}
-                    </div>
-                  )}
+                  {option.values.map((optValue) => (
+                    <Swatch
+                      key={`${option.id}-${optValue.label}`}
+                      label={optValue.label}
+                      color={optValue.hexColor}
+                    />
+                  ))}
                 </div>
               </div>
-            )}
+            ))}
             <div className="pb-14 break-words w-full max-w-xl text-lg">
-              { product.description }
+              {product.description}
             </div>
           </section>
           <div>
             <Button
               className={s.button}
-              onClick={() => alert("adding to cart")}>
+              onClick={() => alert("adding to cart")}
+            >
               Add to Cart
             </Button>
           </div>
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default ProductView
+export default ProductView;
