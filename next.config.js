7cafe08d6526567bff8fdd9596aca23438f37051
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-11-06 11:43:29 +0200

    Adds API url const

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const { withFrameworkConfig } = require("./src/framework/common/config")
 
 module.exports = withFrameworkConfig({
   framework: {
-    name: "shopify_local"
+    name: process.env.NEXT_PUBLIC_FRAMEWORK
   },
   i18n: {
     locales: ["en-US", "es"],
